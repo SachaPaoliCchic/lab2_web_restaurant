f2e22ab567013a64da14a8d222882f4eef1a9033
SachaPaoli <sacha.paoli05@gmail.com>
2025-09-11 16:25:11 +0200

    Add initial migration and views for reservation system; remove unused files

--- demo_app/views.py
+++ demo_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
+class IndexView(TemplateView):
+    template_name = "index.html"
+
+class MenuView(TemplateView):
+    template_name = "menu.html"
+
+class ReservationClientView(TemplateView):
+    template_name = "reservation_client.html"
+
+class ReservationListView(TemplateView):
+    template_name = "reservations.html"
