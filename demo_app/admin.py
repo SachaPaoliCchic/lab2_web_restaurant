2f42f9f3df1acc7d2a135a3c074e5dc6d14f39c9
SachaPaoli <sacha.paoli05@gmail.com>
2025-09-29 18:17:11 +0200

    feat: enhance admin interface with custom model admin classes for Client, Table, and Reservation

--- demo_app/admin.py
+++ demo_app/admin.py
@@ -1,6 +1,24 @@
 from django.contrib import admin
 from .models import Client, Table, Reservation
 
-admin.site.register(Client)
-admin.site.register(Table)
-admin.site.register(Reservation)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'email')
+    search_fields = ('nom', 'email')
+    ordering = ('nom',)
+    readonly_fields = ('email',)
+
+class TableAdmin(admin.ModelAdmin):
+    list_display = ('numero', 'capacite')
+    search_fields = ('numero',)
+    ordering = ('numero',)
+
+class ReservationAdmin(admin.ModelAdmin):
+    list_display = ('client', 'table', 'date', 'nombre_personnes')
+    search_fields = ('client__nom', 'table__numero')
+    list_filter = ('date', 'table')
+    ordering = ('-date',)
+    exclude = ('nombre_personnes',)  
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Table, TableAdmin)
+admin.site.register(Reservation, ReservationAdmin)
